55344e53774156040bc42cfc5da46d30d3fe10a6
clay <jmckittrick@comcast.net>
2021-07-29 17:59:51 -0400

    Initial data

---
+++ initialize_data.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+from components.models import *
+import os
+
+obj_list = {'MeasuringUnit': MeasuringUnit, 'Aisle': Aisle, 'FoodCategory': FoodCategory, 'RecipeCategory': RecipeCategory}
+
+with open('initial_data.txt','r') as f:
+	for x in f:
+		tmp = x.split(',')
+		with open(f'{os.getcwd()}/InitialData/{tmp[1]}','r') as base_file:
+			for line in base_file:
+				obj_list[tmp[0]].objects.create(tmp[2]=line).save()
+				#y(tmp[2]=line).save()
+				#with open('start_data.txt','a') as start:
+				#	start.write(f'{tmp[0]}({tmp[2]}={line}).save()')
+				#	start.write('\n')
